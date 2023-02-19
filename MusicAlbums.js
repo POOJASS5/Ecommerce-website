b097fa27b6473fe3aea973d9d284db6ea315250c
POOJA <121348592+poojaANUP@users.noreply.github.com>
2023-02-19 23:22:20 +0530

    creating the product details dynamic routing

--- MusicAlbums.js
+++ MusicAlbums.js
@@ -2,20 +2,18 @@
 
 
 import React from "react";
-
 import classes from "./MusicAlbums.module.css";
 import MusicItems from "./MusicItems";
-
 const MusicAlbums = (props) => {
   const musicItemList = props.productList.map((item) => (
     <MusicItems key={Math.random().toString()} item={item} />
   ));
 
   return (
-    <React.Fragment>
+    <div className={classes.div}>
       <h2>MUSIC</h2>
       <div className={classes.musicList}>{musicItemList}</div>
-    </React.Fragment>
+    </div>
   );
 };
 
